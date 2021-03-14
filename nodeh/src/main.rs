d51a34c2aebf6ab8e5c807190d5324eb36404d6e
Levi Harrison <levisamuelharrison@gmail.com>
2021-03-14 16:01:36 -0400

    Worked on custom parser implementation

--- nodeh/src/main.rs
+++ nodeh/src/main.rs
@@ -1,18 +1,14 @@
-use html5ever::tendril::TendrilSink;
 use nodeh::parser;
 
-use std::collections::HashMap;
+use html5ever::tendril::TendrilSink;
+
 use std::default::Default;
 use std::path::Path;
 
 use html5ever::parse_document;
 
 fn main() {
-    let sink = parser::Sink {
-        next_id: 1,
-        names: HashMap::new(),
-        line: 0,
-    };
+    let sink = parser::Parser::new();
 
     parse_document(sink, Default::default())
         .from_utf8()
--- nodeh/src/parser.rs
+++ nodeh/src/parser.rs
@@ -3,118 +3,102 @@ use std::collections::HashMap;
 
 use html5ever::tendril::*;
 use html5ever::tree_builder::{
-    AppendNode, AppendText, ElementFlags, NodeOrText, QuirksMode, TreeBuilderOpts, TreeSink,
+    AppendNode, AppendText, ElementFlags, NodeOrText, QuirksMode, TreeSink,
 };
 use html5ever::{
-    expanded_name, local_name, namespace_url, ns, Attribute, ExpandedName, ParseOpts, QualName,
+    expanded_name, local_name, namespace_url, ns, Attribute, ExpandedName, LocalName, QualName,
 };
 
-pub struct Sink {
-    pub next_id: usize,
-    pub names: HashMap<usize, QualName>,
-    pub line: u64,
+type Handle = usize;
+
+struct Element {
+    element_name: String,
+    parent: Handle,
+    parent_func: Handle,
+    qual_name: QualName,
 }
 
-impl Sink {
-    fn get_id(&mut self) -> usize {
+#[derive(Default)]
+pub struct Parser {
+    next_id: Handle,
+    line: u64,
+    elements: HashMap<Handle, Element>,
+}
+
+impl Parser {
+    fn get_id(&mut self) -> Handle {
         let id = self.next_id;
         self.next_id += 2;
         id
     }
+
+    pub fn new() -> Parser {
+        Parser {
+            next_id: 1,
+            line: 0,
+            elements: HashMap::new(),
+        }
+    }
 }
 
-impl TreeSink for Sink {
+impl TreeSink for Parser {
     type Handle = usize;
     type Output = Self;
 
     fn finish(self) -> Self {
-        println!("done");
         self
     }
 
     fn parse_error(&mut self, msg: Cow<'static, str>) {
         match msg {
-            Cow::Borrowed("Bad DOCTYPE") => println!("Not bad doctype"),
+            Cow::Borrowed("Bad DOCTYPE") => {}
             _ => {
-                eprintln!("Error Parsing on line {}", self.line - 1);
+                eprintln!("Error Parsing on line {}", self.line);
                 std::process::exit(1)
             }
         }
     }
 
-    fn get_document(&mut self) -> usize {
+    fn get_document(&mut self) -> Handle {
         0
     }
 
-    fn get_template_contents(&mut self, target: &usize) -> usize {
-        if let Some(expanded_name!(html "template")) = self.names.get(target).map(|n| n.expanded())
-        {
-            target + 1
-        } else {
-            panic!("not a template element")
-        }
+    fn elem_name(&self, target: &Handle) -> ExpandedName {
+        self.elements
+            .get(target)
+            .expect("No element found")
+            .qual_name
+            .expanded()
     }
 
-    fn set_quirks_mode(&mut self, mode: QuirksMode) {
-        println!("Set quirks mode to {:?}", mode);
-    }
-
-    fn same_node(&self, x: &usize, y: &usize) -> bool {
-        x == y
-    }
-
-    fn elem_name(&self, target: &usize) -> ExpandedName {
-        self.names.get(target).expect("not an element").expanded()
-    }
-
-    fn create_element(
-        &mut self,
-        name: QualName,
-        attributes: Vec<Attribute>,
-        _: ElementFlags,
-    ) -> usize {
+    fn create_element(&mut self, name: QualName, attrs: Vec<Attribute>, _: ElementFlags) -> Handle {
         let id = self.get_id();
-        println!("Created {:?} as {} with {:#?}", name, id, attributes);
-        self.names.insert(id, name);
+        let element = Element {
+            element_name: name.local.to_string(),
+            parent: 0,
+            parent_func: 0,
+            qual_name: name,
+        };
+        self.elements.insert(id, element);
         id
     }
 
-    fn create_comment(&mut self, text: StrTendril) -> usize {
-        let id = self.get_id();
-        println!("Created comment \"{}\" as {}", escape_default(&text), id);
-        id
-    }
-
-    #[allow(unused_variables)]
-    fn create_pi(&mut self, target: StrTendril, value: StrTendril) -> usize {
+    fn create_comment(&mut self, _: StrTendril) -> Handle {
         unimplemented!()
     }
 
-    fn append(&mut self, parent: &usize, child: NodeOrText<usize>) {
-        match child {
-            AppendNode(n) => println!("Append node {} to {}", n, parent),
-            AppendText(t) => println!(
-                "Append text to {:#?}: \"{}\"",
-                self.names[parent],
-                escape_default(&t)
-            ),
-        }
+    fn create_pi(&mut self, _: StrTendril, _: StrTendril) -> Handle {
+        unimplemented!()
     }
 
-    fn append_before_sibling(&mut self, sibling: &usize, new_node: NodeOrText<usize>) {
-        match new_node {
-            AppendNode(n) => println!("Append node {} before {}", n, sibling),
-            AppendText(t) => println!("Append text before {}: \"{}\"", sibling, escape_default(&t)),
-        }
-    }
+    fn append(&mut self, parent: &Handle, child: NodeOrText<Handle>) {}
 
     fn append_based_on_parent_node(
         &mut self,
-        element: &Self::Handle,
-        _prev_element: &Self::Handle,
-        child: NodeOrText<Self::Handle>,
+        element: &Handle,
+        prev_element: &Handle,
+        child: NodeOrText<Handle>,
     ) {
-        self.append_before_sibling(element, child);
     }
 
     fn append_doctype_to_document(
@@ -123,49 +107,27 @@ impl TreeSink for Sink {
         public_id: StrTendril,
         system_id: StrTendril,
     ) {
-        println!("Append doctype: {} {} {}", name, public_id, system_id);
     }
 
-    fn add_attrs_if_missing(&mut self, target: &usize, attrs: Vec<Attribute>) {
-        assert!(self.names.contains_key(target), "not an element");
-        println!("Add missing attributes to {}:", target);
-        for attr in attrs.into_iter() {
-            println!("    {:?} = {}", attr.name, attr.value);
-        }
+    fn get_template_contents(&mut self, target: &Handle) -> Handle {
+        unimplemented!()
     }
 
-    fn associate_with_form(
-        &mut self,
-        _target: &usize,
-        _form: &usize,
-        _nodes: (&usize, Option<&usize>),
-    ) {
-        // No form owner support.
+    fn same_node(&self, x: &Handle, y: &Handle) -> bool {
+        x == y
     }
 
-    fn remove_from_parent(&mut self, target: &usize) {
-        println!("Remove {} from parent", target);
-    }
+    fn set_quirks_mode(&mut self, mode: QuirksMode) {}
 
-    fn reparent_children(&mut self, node: &usize, new_parent: &usize) {
-        println!("Move children from {} to {}", node, new_parent);
-    }
+    fn append_before_sibling(&mut self, sibling: &Handle, new_node: NodeOrText<Handle>) {}
 
-    fn mark_script_already_started(&mut self, node: &usize) {
-        println!("Mark script {} as already started", node);
-    }
+    fn add_attrs_if_missing(&mut self, target: &Handle, attrs: Vec<Attribute>) {}
 
-    fn set_current_line(&mut self, line_number: u64) {
-        println!("Set current line to {}", line_number);
-        self.line = line_number;
-    }
+    fn remove_from_parent(&mut self, target: &Handle) {}
 
-    fn pop(&mut self, elem: &usize) {
-        println!("Popped element {}", elem);
-    }
-}
+    fn reparent_children(&mut self, node: &Handle, new_parent: &Handle) {}
 
-// FIXME: Copy of str::escape_default from std, which is currently unstable
-pub fn escape_default(s: &str) -> String {
-    s.chars().flat_map(|c| c.escape_default()).collect()
+    fn set_current_line(&mut self, _line_number: u64) {
+        self.line = _line_number;
+    }
 }
